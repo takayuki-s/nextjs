a44a7692a9bd1c681c4338a312b1bfe01787d4fd
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-24 06:06:23 +0900

    トップページのレイアウトを更新
    今はハードコーディングで代用

--- nextjs-microblog/pages/index.js
+++ nextjs-microblog/pages/index.js
@@ -4,7 +4,30 @@ import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 
 import Link from "next/link";
+import utilstyle from "../styles/utils.module.css";
 
 export default function Home() {
-  return <Layout></Layout>;
+  return (
+    <Layout>
+      <section className={utilstyle.headingMd}>
+        <p>プロフィールのテキストサンプルです！</p>
+      </section>
+
+      <section>
+        <h2>📝エンジニアのブログ</h2>
+        <div>
+          <article>
+            <Link href="/">
+              <img src="/images/thumbnail01.jpg" alt="" />
+            </Link>
+            <Link href="/">
+              <a>SSGとSSRの使い分けの場面はいつなのか？</a>
+            </Link>
+            <br />
+            <small>February 23, 2022</small>
+          </article>
+        </div>
+      </section>
+    </Layout>
+  );
 }
