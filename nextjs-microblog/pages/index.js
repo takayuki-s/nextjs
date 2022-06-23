25af8be76b4641a344c756947581b52af599c3c6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-24 06:12:32 +0900

    トップページのスタイリングを調整

--- nextjs-microblog/pages/index.js
+++ nextjs-microblog/pages/index.js
@@ -4,27 +4,77 @@ import styles from "../styles/Home.module.css";
 import Layout from "../components/Layout";
 
 import Link from "next/link";
-import utilstyle from "../styles/utils.module.css";
+import utilStyle from "../styles/utils.module.css";
 
 export default function Home() {
   return (
     <Layout>
-      <section className={utilstyle.headingMd}>
+      <section className={utilStyle.headingMd}>
         <p>プロフィールのテキストサンプルです！</p>
       </section>
 
       <section>
         <h2>📝エンジニアのブログ</h2>
-        <div>
+        <div className={styles.grid}>
           <article>
             <Link href="/">
-              <img src="/images/thumbnail01.jpg" alt="" />
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
             </Link>
             <Link href="/">
-              <a>SSGとSSRの使い分けの場面はいつなのか？</a>
+              <a className={utilStyle.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
             </Link>
             <br />
-            <small>February 23, 2022</small>
+            <small className={utilStyle.lightText}>February 23, 2022</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyle.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2022</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyle.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2022</small>
+          </article>
+          <article>
+            <Link href="/">
+              <img
+                src="/images/thumbnail01.jpg"
+                className={styles.thumbnailImage}
+              />
+            </Link>
+            <Link href="/">
+              <a className={utilStyle.boldText}>
+                SSGとSSRの使い分けの場面はいつなのか？
+              </a>
+            </Link>
+            <br />
+            <small className={utilStyle.lightText}>February 23, 2022</small>
           </article>
         </div>
       </section>
