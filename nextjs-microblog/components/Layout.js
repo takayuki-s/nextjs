b40d4a20c8be6db855a9788d4ab6c677068901ef
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-24 05:51:17 +0900

    タイポグラフィに関するCSSモジュールを用意

--- nextjs-microblog/components/Layout.js
+++ nextjs-microblog/components/Layout.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "./layout.module.css";
+import utilstyles from "../styles/utils.module.css";
 
 const name = "Teke Code";
 export const siteTitle = "Next.js Blog";
@@ -11,8 +12,8 @@ function Layout({ children }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="/images/profile.png" />
-        <h1>{name}</h1>
+        <img src="/images/profile.png" className={utilstyles.borderCircle} />
+        <h1 className={utilstyles.heading2Xl}>{name}</h1>
       </header>
       <main>{children}</main>
     </div>
