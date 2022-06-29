40f53da7242dfa25d7a10fc5bb0290f81697b876
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-06-29 09:18:03 +0900

    ブログの細かい訂正（アイコン画像）
    Homeコンポーネント を読み込んでいる時は少し大きめのアイコン画像を表示

--- nextjs-microblog/components/Layout.js
+++ nextjs-microblog/components/Layout.js
@@ -5,15 +5,30 @@ import utilstyles from "../styles/utils.module.css";
 const name = "Teke Code";
 export const siteTitle = "Next.js Blog";
 
-function Layout({ children }) {
+function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <header className={styles.header}>
-        <img src="/images/profile.png" className={utilstyles.borderCircle} />
-        <h1 className={utilstyles.heading2Xl}>{name}</h1>
+        {home ? (
+          <>
+            <img
+              src="/images/profile.png"
+              className={`${utilstyles.borderCircle} ${styles.headerHomeImage}`}
+            />
+            <h1 className={utilstyles.heading2Xl}>{name}</h1>
+          </>
+        ) : (
+          <>
+            <img
+              src="/images/profile.png"
+              className={`${utilstyles.borderCircle}`}
+            />
+            <h1 className={utilstyles.heading2Xl}>{name}</h1>
+          </>
+        )}
       </header>
       <main>{children}</main>
     </div>
--- nextjs-microblog/pages/index.js
+++ nextjs-microblog/pages/index.js
@@ -21,7 +21,7 @@ export async function getStaticProps() {
 
 export default function Home({ allPostsData }) {
   return (
-    <Layout>
+    <Layout home>
       <section className={utilStyle.headingMd}>
         <p>プロフィールのテキストサンプルです！</p>
       </section>
