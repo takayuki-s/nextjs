cd8cee8a203bbcbaf05db96078af7684600eff93
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-08-08 08:16:39 +0900

    SSRページ作成

---
+++ next-todo-app/pages/ssr.tsx
@@ -0,0 +1,65 @@
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { NextPage, GetServerSideProps } from 'next'
+import { supabase } from '../utils/supabase'
+import { Notice, Task } from '../types/types'
+import { Layout } from '../components/Layout'
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  console.log('getServerSideProps/ssr invoked')
+  const { data: tasks } = await supabase
+    .from('todos')
+    .select('*')
+    .order('created_at', { ascending: true })
+  const { data: notices } = await supabase
+    .from('notices')
+    .select('*')
+    .order('created_at', { ascending: true })
+  return { props: { tasks, notices } }
+}
+
+type StaticProps = {
+  tasks: Task[]
+  notices: Notice[]
+}
+
+const Ssr: NextPage<StaticProps> = ({ tasks, notices }) => {
+  const router = useRouter()
+  return (
+    <Layout title="SSR">
+      <p className="mb-3 text-pink-500">SSR</p>
+      <ul className="mb-3">
+        {tasks.map((task) => {
+          return (
+            <li key={task.id}>
+              <p className="text-lg font-extrabold">{task.title}</p>
+            </li>
+          )
+        })}
+      </ul>
+      <ul className="mb-3">
+        {notices.map((notice) => {
+          return (
+            <li key={notice.id}>
+              <p className="text-lg font-extrabold">{notice.content}</p>
+            </li>
+          )
+        })}
+      </ul>
+      <Link href="/ssg" prefetch={false}>
+        <a className="my-3 text-xs"> Link to ssg</a>
+      </Link>
+      <Link href="/isr" prefetch={false}>
+        <a className="mb-3 text-xs"> Link to isr</a>
+      </Link>
+      <button className="mb-3 text-xs" onClick={() => router.push('ssg')}>
+        Route to ssg
+      </button>
+      <button className="mb-3 text-xs" onClick={() => router.push('isr')}>
+        Route to isr
+      </button>
+    </Layout>
+  )
+}
+
+export default Ssr
