37a21b84128b764f204036d8919c7466f4630a22
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-08-08 08:16:10 +0900

    不要コード削除

--- next-todo-app/pages/index.tsx
+++ next-todo-app/pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import { Layout } from '../components/Layout'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return <Layout title="Home">{'hello'}</Layout>
