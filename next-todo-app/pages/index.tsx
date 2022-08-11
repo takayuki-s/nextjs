8b0b4c8739b3ed68380ab97793f37dc088fbf54e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2022-08-12 07:13:47 +0900

    プロジェクト移動

--- next-todo-app/pages/index.tsx
+++
@@ -1,8 +0,0 @@
-import type { NextPage } from 'next'
-import { Layout } from '../components/Layout'
-
-const Home: NextPage = () => {
-  return <Layout title="Home">{'hello'}</Layout>
-}
-
-export default Home
